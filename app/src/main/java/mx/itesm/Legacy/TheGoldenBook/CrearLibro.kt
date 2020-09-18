2f391e46cca815956f2594090d71c4969b2f14e8
Ricardo <ricardov_5678@hotmail.com>
2020-09-18 15:08:48 -0500

    Se agregan las vistas de nuevo libro y editar libro

---
+++ app/src/main/java/mx/itesm/Legacy/TheGoldenBook/CrearLibro.kt
@@ -0,0 +1,11 @@
+package mx.itesm.Legacy.TheGoldenBook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearLibro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_libro)
+    }
+}
