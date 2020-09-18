1a7d3360ac8bde51c71c9d7c8da49b67cd5424d2
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2020-09-18 15:24:40 -0500

    Pantallas Avance 1 completas con cambio de pantallas funcionando

--- app/src/main/java/mx/itesm/Legacy/TheGoldenBook/ActividadMenu2.kt
+++ app/src/main/java/mx/itesm/Legacy/TheGoldenBook/ActividadMenu2.kt
@@ -6,12 +6,30 @@
 
 package mx.itesm.Legacy.TheGoldenBook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ActividadMenu2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu2)
+    }
+
+    fun crearNuevoClicked(view: View){
+        val intentCrearNuevo = Intent(this, CrearLibro::class.java).apply {
+
+        }
+
+        startActivity(intentCrearNuevo)
+    }
+
+    fun editarClicked(view: View){
+        val intentEditar = Intent(this, EditarLibro::class.java).apply {
+
+        }
+
+        startActivity(intentEditar)
     }
 }
