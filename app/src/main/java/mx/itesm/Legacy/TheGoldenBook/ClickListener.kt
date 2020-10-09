ff2283f147922360ee28ffbfbca0e74d5a8620d0
Ricardo <ricardov_5678@hotmail.com>
2020-10-09 14:48:51 -0500

    Se agregan el RV para mostrar la lista de libros y el esqueleto para ir a consultar la información a la base de datos

---
+++ app/src/main/java/mx/itesm/Legacy/TheGoldenBook/ClickListener.kt
@@ -0,0 +1,5 @@
+package mx.itesm.Legacy.TheGoldenBook
+
+interface ClickListener {
+    fun clicked(posicion : Int)
+}
--- app/src/main/java/mx/itesm/Legacy/TheGoldenBook/EditarLibro.kt
+++ app/src/main/java/mx/itesm/Legacy/TheGoldenBook/EditarLibro.kt
@@ -2,10 +2,35 @@ package mx.itesm.Legacy.TheGoldenBook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_editar_libro.*
 
-class EditarLibro : AppCompatActivity() {
+class EditarLibro : AppCompatActivity(), ClickListener {
+    private val editarLibroController : EditarLibroController = EditarLibroController()
+    lateinit var arrLibros: Array<Libro>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editar_libro)
+
+        configurarRecycleView()
+    }
+
+    private fun configurarRecycleView() {
+        val admLayout =LinearLayoutManager(this)
+
+        arrLibros = editarLibroController.getArrLibrosBD("")
+
+        val adaptador= AdaptadorListaLibros(arrLibros)
+
+        rvLibros.layoutManager =admLayout
+        rvLibros.adapter = adaptador
+
+        adaptador.listener = this
+    }
+
+    override fun clicked(posicion: Int) {
+        TODO("Not yet implemented")
     }
 }
