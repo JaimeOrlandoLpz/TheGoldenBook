369fceb4603016e648fe48c3022232bce9e0c92d
Ricardo <ricardov_5678@hotmail.com>
2020-10-13 22:24:34 -0500

    Se agrega la funcionalidad para bajar inmformación de la base

--- app/src/main/java/mx/itesm/Legacy/TheGoldenBook/EditarLibroController.kt
+++ app/src/main/java/mx/itesm/Legacy/TheGoldenBook/EditarLibroController.kt
@@ -1,11 +1,37 @@
 package mx.itesm.Legacy.TheGoldenBook
 
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 class EditarLibroController {
 
      fun getArrLibrosBD(idUsuario : String) : Array<Libro>{
 
-        return arrayOf(
-            Libro("Prueba", "Prueba","01/01/2020")
-        )
+         val baseDatos = FirebaseDatabase.getInstance()
+         val referencia = baseDatos.getReference("/Libro")
+         referencia.keepSynced(true)
+         var lsLibros = mutableListOf<Libro>()
+
+         referencia.addListenerForSingleValueEvent(object : ValueEventListener {
+
+             override fun onDataChange(snapshot : DataSnapshot) {
+                 for (registro in snapshot.children){
+                    val libro = registro.getValue(Libro :: class.java)
+
+                    if(libro != null){
+                        lsLibros.add(libro)
+                    }
+                 }
+             }
+
+             override fun onCancelled(error: DatabaseError) {
+
+             }
+         })
+         println(lsLibros)
+
+         return lsLibros.toTypedArray()
     }
 }
