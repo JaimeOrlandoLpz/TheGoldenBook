3b5b4ac17ec2c1135a9c2a2c7a97061a42d2aee0
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2020-10-25 01:37:58 -0600

    Se agrega Menu Para Acerca de

--- app/src/main/java/mx/itesm/Legacy/TheGoldenBook/ActividadMenu1.kt
+++ app/src/main/java/mx/itesm/Legacy/TheGoldenBook/ActividadMenu1.kt
@@ -3,12 +3,16 @@ package mx.itesm.Legacy.TheGoldenBook
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView
 
 class ActividadMenu1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu1)
+
     }
 
     fun loginClicked(view: View){
@@ -19,6 +23,12 @@ class ActividadMenu1 : AppCompatActivity() {
         startActivity(intentLog)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean{
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu_overflow, menu)
+        return true
+    }
+
     fun registroClick(view: View) {
         val intentReg = Intent(this, ActividadCrearCuenta::class.java).apply {
 
@@ -27,11 +37,17 @@ class ActividadMenu1 : AppCompatActivity() {
         startActivity(intentReg)
     }
 
-    fun acercaClick(view: View) {
-        val intentReg = Intent(this, Ayuda::class.java).apply {
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.acercaDe) {
+            val intentReg = Intent(this, Ayuda::class.java).apply {
+            }
+            startActivity(intentReg)
+            return true
         }
-
-        startActivity(intentReg)
+        return super.onContextItemSelected(item)
     }
+
+
+
 }
